e1facf7c62a1db50dd967b697e7161850902040c
Sujeet Yadav <androexp3rt@gmail.com>
2024-09-06 15:06:06 +0530

    shorts added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,10 +2,10 @@
 import { useState,useEffect } from "react";
 
 export default function Home() {
-  const [vidId,setVidId] = useState("JVMFHP8CxZQ");
+  const [vidId,setVidId] = useState("7utxCRasgKc");
   const [currentIdIndex, setCurrentIdIndex] = useState(0);
-  const [ids,setIds] = useState(["JVMFHP8CxZQ","xGIIQOFwUNc","uP79KgEYGN0","sUoy0om-BvY"]);
-  const [time,setTime] = useState([840000,780000,3060000,2280000]);
+  const [ids,setIds] = useState(["7utxCRasgKc","JVMFHP8CxZQ","biZBRR_cv9o","xGIIQOFwUNc","C8woWtVN3_g","uP79KgEYGN0","IOP4Yp080bw","sUoy0om-BvY"]);
+  const [time,setTime] = useState([60000,840000,60000,780000,60000,3060000,60000,2280000]);
   // const [time,setTime] = useState([60000,60000,60000]);
   function update() {
     const intervalId = setInterval(() => {
